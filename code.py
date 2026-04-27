69b8ebb7938026ed1d4800a97e3ca4f5d1e1f746
Ahmad Hassan <ahmad.hassan.egy.dev@gmail.com>
2026-04-27 14:38:34 +0300

    Loops

--- code.py
+++ code.py
@@ -1,8 +1,4 @@
 if __name__ == '__main__':
-    a = int(input())
-    b = int(input())
-    def Arithmetic_operators(a,b):
-        print(a+b)
-        print(a-b)
-        print(a*b)
-    Arithmetic_operators(a,b)    
+    n = int(input())
+    for i in range(n):
+        print(i**2)
